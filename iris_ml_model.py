144f963a41bff3d0dd2563c377cf8d76eb57858e
Seshu0208 <seshubabu0208@gmail.com>
2026-01-07 12:08:59 +0530

    Add files via upload
    iris_knn_model

---
+++ iris_ml_model.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target
+# print(df.head())
+
+print(df["target"].unique())
+# print(len(df[df["target"]==0]))
+# print(len(df[df["target"]==1]))
+# print(len(df[df["target"]==2]))
+
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+
+k = 5
+
+knn = KNeighborsClassifier(n_neighbors=k)
+
+knn.fit(X_train,y_train)
+
+y_pred = knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(accuracy)
+
+
+print(classification_report(y_test, y_pred))
+
+
